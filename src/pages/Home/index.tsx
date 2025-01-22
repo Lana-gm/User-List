4065f63c7a3e2f39a3e6c3ab34b5b411983c9085
Lana Marchetti <lanagmarchetti@gmail.com>
2025-01-22 16:39:23 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import UsersList from "../../components/UserList";
+import * as S from "./Home.styles";
+
+const Home = () => {
+  return (
+    <S.Container>
+      <UsersList />
+    </S.Container>
+  );
+};
+
+export default Home;
